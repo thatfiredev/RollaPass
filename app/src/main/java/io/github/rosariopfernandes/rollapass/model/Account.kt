815b68442084322c69da708c41646f82a9e4dbd2
Rosário Pereira Fernandes <rosariofernandes51@gmail.com>
2018-05-29 23:15:04 +0200

    Created model data classes Created the User and Account classes

---
+++ app/src/main/java/io/github/rosariopfernandes/rollapass/model/Account.kt
@@ -0,0 +1,4 @@
+package io.github.rosariopfernandes.rollapass.model
+
+data class Account(val accountId:Int, val userName:String, val userPassword:String,
+                   val userWebsite:String)
---
+++ app/src/main/java/io/github/rosariopfernandes/rollapass/model/User.kt
@@ -0,0 +1,3 @@
+package io.github.rosariopfernandes.rollapass.model
+
+data class User(val masterPassword:String)
